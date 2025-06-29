ab99c1b3fd8a2a469489b386118e47bd8f6da04c
Kartik Ramesh <kartikr2@illinois.edu>
2025-06-29 13:57:29 -0700

    basic server

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	time.Sleep(1 * time.Second)
+	fmt.Fprintln(w,"hello");
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.ListenAndServe(":8080", nil)
+}
