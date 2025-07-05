9fcb2eaf3c6c61152dcd98f858b92ef9ddcfb378
Kartik Ramesh <kartikr2@illinois.edu>
2025-07-05 15:30:51 -0700

    kind service

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(w, "Hello")
+	hostname, _ := os.Hostname()
+    fmt.Fprintf(w, "Hello from %s\n", hostname)
 }
 
 func main() {
